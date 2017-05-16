07c0b0cf42625a4e3a04a09459e33e51015079bb
Marcin Kacik <marcinkacik@interia.pl>
2017-05-17 00:24:06 +0200

    Fixed some problems with actions on list, added popup Y/N window

--- Ajwon/src/main/java/com/apps/szpansky/ajwon_app/OpenWorksActivity.java
+++ Ajwon/src/main/java/com/apps/szpansky/ajwon_app/OpenWorksActivity.java
@@ -1,5 +1,7 @@
 package com.apps.szpansky.ajwon_app;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
@@ -13,29 +15,48 @@ import android.widget.SimpleCursorAdapter;
 
 public class OpenWorksActivity extends AppCompatActivity {
 
-Database myDB = new Database(this);
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        myDB = new Database(this);
+        cursor = myDB.getAllRows(Database.TABLE_WORKS, Database.ALL_KEYS_WORK, Database.WORK_CATALOG_NR);
+        fromFieldsNames = Database.ALL_KEYS_WORK;
+        toViewIDs = new int[]{R.id.catalogId, R.id.date};
+        myCursorAdapter = new SimpleCursorAdapter(getBaseContext(), R.layout.item_work_view, cursor, fromFieldsNames, toViewIDs, 0);
+        workView = (ListView) findViewById(R.id.list_view_catalogs);
+    }
+
+
+    Database myDB;
+    Cursor cursor;
+    String[] fromFieldsNames;
+    int[] toViewIDs;
+    SimpleCursorAdapter myCursorAdapter;
+    ListView workView;
 
 
     @Override
     protected void onPostResume() {
         super.onPostResume();
-        populateListView();
+        workView.setAdapter(myCursorAdapter);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_works);
+
         addData();
-        populateListView();
-        listViewItemDelClick();
         listViewItemClick();
+
     }
 
 
-
-    public void addData(){
+    public void addData() {
         Button add = (Button) findViewById(R.id.add);
+
         add.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -47,47 +68,56 @@ Database myDB = new Database(this);
     }
 
 
-    private void populateListView(){
-        Cursor cursor = myDB.getAllRows(Database.TABLE_WORKS,Database.ALL_KEYS_WORK, Database.WORK_CATALOG_NR);
-        String[] fromFieldsNames = Database.ALL_KEYS_WORK;
-        int[] toViewIDs = new int[] {R.id.catalogId, R.id.date};
-        SimpleCursorAdapter myCursorAdapter;
-        myCursorAdapter = new SimpleCursorAdapter(getBaseContext(), R.layout.item_work_view, cursor, fromFieldsNames, toViewIDs, 0);
-        ListView workView = (ListView) findViewById(R.id.list_view_catalogs);
-        workView.setAdapter(myCursorAdapter);
+    private void popup(final long id) {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        myDB.delete(Database.TABLE_WORKS, id, Database.WORK_CATALOG_NR);
+                        finish();
+                        startActivity(getIntent());
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("You want to delete?").setPositiveButton("Yes", dialogClickListener)
+                .setNegativeButton("No", dialogClickListener).show();
     }
 
-    private void listViewItemDelClick(){
+
+    private void listViewItemClick() {
         final ListView myList = (ListView) findViewById(R.id.list_view_catalogs);
+        final Boolean[] flag = new Boolean[1];
+        flag[0] = true;
+
         myList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                myDB.delete(Database.TABLE_WORKS,id,Database.WORK_CATALOG_NR);
-                populateListView();
+                flag[0] = false;
+                popup(id);
                 return false;
             }
         });
 
-    }
-
-
-    private void listViewItemClick(){
-        final ListView myList = (ListView) findViewById(R.id.list_view_catalogs);
-        myList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                Intent Intent_Open_Orders = new Intent(OpenWorksActivity.this, OpenOrdersActivity.class );
-
-                Bundle b = new Bundle();
-                b.putLong("id", id);
-                Intent_Open_Orders.putExtras(b);
-
-                OpenWorksActivity.this.startActivity(Intent_Open_Orders);
-
+                if (flag[0]) {
+                    Intent Intent_Open_Orders = new Intent(OpenWorksActivity.this, OpenOrdersActivity.class);
+                    Bundle b = new Bundle();
+                    b.putLong("id", id);
+                    Intent_Open_Orders.putExtras(b);
+                    OpenWorksActivity.this.startActivity(Intent_Open_Orders);
+                }
+                flag[0] = true;
             }
         });
     }
-
-
 }
